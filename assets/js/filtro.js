89f09290295397fa2dd66c9d6bc45a0d4254e793
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2022-09-30 19:12:33 -0500

    Filtros y productos

---
+++ assets/js/filtro.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
+
+	// FILTRANDO PRODUCTOS  ============================================
+
+	$('.category_item').click(function(){
+		var catProduct = $(this).attr('category');
+		console.log(catProduct);
+
+		var catProduct2 = $(this).attr('category2');
+		console.log(catProduct2);
+
+		var catProduct3 = $(this).attr('category3');
+		console.log(catProduct3);
+
+		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+		$('.category_item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+		// OCULTANDO PRODUCTOS =========================
+		$('.product-item').css('transform', 'scale(0)');
+		function hideProduct(){
+			$('.product-item').hide();
+		} setTimeout(hideProduct,400);
+
+		// MOSTRANDO PRODUCTOS =========================
+		function showProduct(){
+			$('.product-item[category="'+catProduct+'"]').show();
+			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+			$('.product-item[category2="'+catProduct2+'"]').show();
+			$('.product-item[category2="'+catProduct2+'"]').css('transform', 'scale(1)');
+			$('.product-item[category3="'+catProduct3+'"]').show();
+			$('.product-item[category3="'+catProduct3+'"]').css('transform', 'scale(1)');
+		} setTimeout(showProduct,400);
+	});
+
+	// MOSTRANDO TODOS LOS PRODUCTOS =======================
+
+	$('.category_item[category="all"]').click(function(){
+		function showAll(){
+			$('.product-item').show();
+			$('.product-item').css('transform', 'scale(1)');
+		} setTimeout(showAll,400);
+	});
+});
