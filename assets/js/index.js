6d868b67a06e82bbb2a3c42efcd5340563ac5df6
Anderson Mendoza <davidmramos53@gmail.com>
2022-10-08 20:12:48 -0500

    agregando script para mostrar/ocultar el buscador

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,3 +49,14 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-pagination',
     },
 });
+
+//show search
+const searchButton = document.querySelector('.t-search'),
+      tClose = document.querySelector('.search-close'),
+      showClass = document.querySelector('.site');
+searchButton.addEventListener('click', function() {
+    showClass.classList.toggle('showsearch');
+})
+tClose.addEventListener('click', function() {
+    showClass.classList.remove('showsearch')
+})
