48733562fb113729b19bdf7aae1f2c02ae71b55f
Anderson Mendoza <davidmramos53@gmail.com>
2022-10-25 15:36:35 -0500

    agregando carrito de compras desplegable

--- js/index.js
+++ js/index.js
@@ -60,3 +60,24 @@ searchButton.addEventListener('click', function() {
 tClose.addEventListener('click', function() {
     showClass.classList.remove('showsearch')
 })
+
+//show cart on click
+const divtoShow = '.mini-cart';
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector('.cart-trigger');
+
+divTrigger.addEventListener('click', () => {
+    setTimeout(() => {
+        if(!divPopup.classList.contains('show')) {
+            divPopup.classList.add('show');
+        }
+    }, 250 )
+})
+
+//close by click outside
+document.addEventListener('click', (e) => {
+    const isClosest = e.target.closest(divtoShow);
+    if(!isClosest && divPopup.classList.contains('show')) {
+        divPopup.classList.remove('show')
+    }
+})
