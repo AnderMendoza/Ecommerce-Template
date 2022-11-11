02fb131e457577d7e77805ea14a11387bf08e238
Jhon Franco Aguilar Rodriguez <73808133@certus.edu.pe>
2022-11-11 00:41:25 -0500

    agregando botones de retroceso

--- js/login-admin.js
+++ js/login-admin.js
@@ -1,3 +1,12 @@
 function loginAdmin(){
     window.location.href = 'login-admin.html';
 }
+function retroceder1(){
+    window.location.href = 'registrar.html';
+}
+function retroceder2(){
+    window.location.href = 'index.html';
+}
+function retroceder3(){
+    window.location.href = 'login.html';
+}
