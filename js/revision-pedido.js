ee10c5b060a8c008b320ab00935b4ee9e65de5c1
Anderson Mendoza <davidmramos53@gmail.com>
2023-01-29 17:54:36 -0500

    Update revision-pedido.js

--- js/revision-pedido.js
+++ js/revision-pedido.js
@@ -28,7 +28,6 @@ closeButton.addEventListener('click', function () {
     addclass.classList.remove('showmenu')
 })
 
-
 //show sub menu on mobile
 const submenu = document.querySelectorAll('.has-child .icon-small');
 submenu.forEach((menu) => menu.addEventListener('click', toggle));
